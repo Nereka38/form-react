028fcd1b83d69ce6833bcff9b669f71a61b84eeb
Nereka38 <87275667+Nereka38@users.noreply.github.com>
2023-01-22 21:59:48 +0100

    add form

---
+++ README.md
@@ -0,0 +1,73 @@
+# 💻React Exercise: Form💻
+
+Small and easy exercise to learn React.js.
+
+
+## ⚙️ How Install This Project
+
+To watch and use this project, only use:
+
+```bash
+    npm init
+    npm start
+```
+ When terminal end with this, is all ok:   
+ ```bash
+    Compiled successfully!
+
+    You can now view project-name in the browser.
+
+    Local:            http://localhost:3000
+    On Your Network:  http://192.168.1.167:3000
+
+    Note that the development build is not optimized.
+    To create a production build, use npm run build.
+
+    webpack compiled successfully
+```
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+### ⚙️ How Install New React Project
+
+If you don't know how to make a project with react from the beginning, it's necessary to do the following steps:
+
+```bash
+    npm init react-app project-name
+    cd project-name
+    npm start
+```
+ The terminal end with:   
+ ```bash
+    Compiled successfully!
+
+    You can now view project-name in the browser.
+
+    Local:            http://localhost:3000
+    On Your Network:  http://192.168.1.167:3000
+
+    Note that the development build is not optimized.
+    To create a production build, use npm run build.
+
+    webpack compiled successfully
+```
+Open a browser with http://localhost:3000, will show the following page in your browser:
+
+![Img](https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started/default-create-react-app.png)
+
+
+## 📚 Documentation
+
+- [React.js](https://es.reactjs.org/)
+- [JavaScript](https://www.javascript.com/)
+- [Babel](https://babeljs.io/)
+- [JSX](https://reactjs.org/docs/introducing-jsx.html)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Node](https://nodejs.org/es/)
+- [npm](https://www.npmjs.com/)
+- [VS Code](https://code.visualstudio.com/)
+
+
+## 🖱 Authors
+
+- [@Nereka38](https://github.com/Nereka38)
---
+++ src/App.js
@@ -0,0 +1,129 @@
+import './App.css'; 
+import {useState} from "react"; 
+import {validateEmail} from "../src/utils"; 
+
+//1. It's necessary convert all form elements into controlled components, assigning each state piece to the value prop from each input element. To be able to account for state updates, each input should also define the onChange prop and call the state setter with the value property from the event target as parameter.
+//2. The isTouched property on the password state was defined to determine when the input was touched at least once. In order to listen for interactions, form inputs have two additional events: onFocus and onBlur. 
+//3. To prevent the default behavior of the form when clicking on the submit button, call preventDefault on the event object, right in the submit handler function.
+//4. To fulfil the validation rules of the form, the body of the getIsFormValid function should be implemented.
+//5. To clear the form state after a successful submission, you should set each piece of state to its initial value.
+const PasswordErrorMessage = () => { 
+ return ( 
+   <p className="FieldError">Password should have at least 8 characters</p> 
+ ); 
+}; 
+ 
+function App() { 
+ const [firstName, setFirstName] = useState(""); 
+ const [lastName, setLastName] = useState(""); 
+ const [email, setEmail] = useState(""); 
+ const [password, setPassword] = useState({ 
+   value: "", 
+   isTouched: false, 
+ }); 
+ const [role, setRole] = useState("role"); 
+
+ const getIsFormValid = () => { 
+   return ( 
+     firstName && 
+     validateEmail(email) && 
+     password.value.length >= 8 && 
+     role !== "role" 
+   ); 
+ }; 
+ 
+ const clearForm = () => { 
+   setFirstName(""); 
+   setLastName(""); 
+   setEmail(""); 
+   setPassword({ 
+     value: "", 
+     isTouched: false, 
+   }); 
+   setRole("role"); 
+ }; 
+ 
+ const handleSubmit = (e) => { 
+   e.preventDefault(); 
+   alert("Account created!"); 
+   clearForm(); 
+ }; 
+ 
+ return ( 
+   <div className="login-box"> 
+     <form onSubmit={handleSubmit}> 
+       <fieldset> 
+         <h2>Sign Up</h2> 
+         <div className="user-box"> 
+           <label> 
+             First name <sup>*</sup> 
+           </label> 
+           <input 
+             value={firstName} 
+             onChange={(e) => { 
+               setFirstName(e.target.value); 
+             }} 
+             placeholder="First name" 
+           /> 
+         </div> 
+         <div className="user-box"> 
+           <label>Last name</label> 
+           <input 
+             value={lastName} 
+             onChange={(e) => { 
+               setLastName(e.target.value); 
+             }} 
+             placeholder="Last name" 
+           /> 
+         </div> 
+         <div className="user-box"> 
+           <label> 
+             Email address <sup>*</sup> 
+           </label> 
+           <input 
+             value={email} 
+             onChange={(e) => { 
+               setEmail(e.target.value); 
+             }} 
+             placeholder="Email address" 
+           /> 
+         </div> 
+         <div className="user-box"> 
+           <label> 
+             Password <sup>*</sup> 
+           </label> 
+           <input 
+             value={password.value} 
+             type="password" 
+             onChange={(e) => { 
+               setPassword({ ...password, value: e.target.value }); 
+             }} 
+             onBlur={() => { 
+               setPassword({ ...password, isTouched: true }); 
+             }} 
+             placeholder="Password" 
+           /> 
+           {password.isTouched && password.value.length < 8 ? ( 
+             <PasswordErrorMessage /> 
+           ) : null} 
+         </div> 
+         <div> 
+           <label> 
+             <sup>*</sup> 
+           </label> 
+           <select value={role} onChange={(e) => setRole(e.target.value)}> 
+             <option value="role">Role</option> 
+             <option value="individual">Individual</option> 
+             <option value="business">Business</option> 
+           </select> 
+         </div> 
+         <button type="submit" disabled={!getIsFormValid()}> 
+           Create account 
+         </button> 
+       </fieldset> 
+     </form> 
+   </div> 
+ ); 
+} 
+
+export default App; 
